12d0cabcfa5c554a4122e95bd0c156b1c5808ba5
Masive <jozollo@gmail.com>
2021-06-18 19:28:00 +0200

    dodaje klase BankAccount

--- src/com/company/BankAccount.java
+++ src/com/company/BankAccount.java
@@ -1,12 +1,50 @@
 package com.company;
 
+// definicja publicznej klasy BancAccount - szablon
 public class BankAccount {
 
+// zmienna typu liczbowego, która bedzie reprezentować ilość pieniędzy
     private int accountBalance;
 
+
+//konstruktor bezparemetrowy - konstruktor uruchamia sie przy tworzeniu instacji klasy
+    // w naszym przypadku to konkretne konto - jadzia, werka;
+    // konstruktor to funkcja ktora nazywa sie jak klasa,
     BankAccount(){
         accountBalance = 0;
+        System.out.println("konstruktor sie odpalil");
+    }
 
+    //konstruktor z parametrem liczbowym o nazwie startingammount
+    //ustawia poczatkowa wartosc accountbalance na przekazana wartosc w argumencie
+    BankAccount(int startingAmount){
+        accountBalance = startingAmount;
+        System.out.println("odpallil sie drugi konstruktor");
 
     }
+
+//publiczna metoda zwracajac booleanm ktora przyjmuje liczbe i zwraca czy mozna wybrac
+    //taka ilosc pieniedzy
+    public boolean withdraw(int amount){
+        if(amount > accountBalance){
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    //publiczna metoda nie zwracajaca wartosci, przyjmuje zmienna liczbowa ammount,
+    //zwieksza aktualny stan konta o podana wartosc
+    public void transfer(int amount){
+        accountBalance = accountBalance + amount;
+    }
+
+//publiczna metoda zwracacaja aktualna wartosc konta
+    public int getAccountBalance(){
+        return accountBalance;
+    }
+
+
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,44 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Siemka swiecie");
+
+//        tworze nowa instancje klasy BankAccount bez przekazania parametrow
+        BankAccount kontoJadzi = new BankAccount();
+
+//        definiuje nowa zmienna kasaJadzi i przypisuje do niej wartosc zwracana przez metode getCurrentBalance w instancj kontoJadzi
+        int kasaJadzi=kontoJadzi.getAccountBalance();
+        System.out.println("obecna ilosc na kontcie jadzi");
+        System.out.println(kasaJadzi);
+
+//definiuje nowa zmienna i przypisuje do niej nowe konkretne konto, ma rozpoczynajaca wartosc 20
+        BankAccount kontoWerki = new BankAccount(20);
+
+        //definiuje nowa zmienna i przypisuje obecna wartosc konta werki
+        int kasaWerki=kontoWerki.getAccountBalance();
+        System.out.println(kasaWerki);
+
+
+        //sprawdzam czy werka moze wyjac 15
+        System.out.println("Czy Werka moze wyciagnac 15");
+        System.out.println(kontoWerki.withdraw(15));
+
+
+        //sprawdzam czy werka moze wyjac 25
+        System.out.println("Czy Werka moze wyciagnac 25");
+        System.out.println(kontoWerki.withdraw(25));
+
+//        dodaje do konta werki 20
+
+        System.out.println("Dodaje do konta werki 20");
+        kontoWerki.transfer(20);
+
+
+        System.out.println("Ile teraz kasy ma werka");
+        System.out.println(kontoWerki.getAccountBalance());
+//        sprawdzam czy moze wyjac 25
+
+        System.out.println("Czy Werka moze wyciagnac 25");
+        System.out.println(kontoWerki.withdraw(25));
 	// write your code here
     }
 }
